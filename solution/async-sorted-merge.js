1414c4bd269531a7dd8c9681a70a81370d3f0133
Hideki Matsumoto <hidekimatsumoto@Hidekis-MacBook-Pro.local>
2024-05-23 18:30:46 -0400

    refactor

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,44 +1,30 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+const { getEarliestLogIndex, isAllDrained } = require('./utils')
 
+// Print all entries, across all of the *async* sources, in chronological order.
 module.exports = async (logSources, printer) => {
 	let currentLogs = []
 
 	// tracking if all the log sources is drained
-	let isAllDrained = false
+	let allDrained = false
 
-	// initialize current logs for each source
+	// Initialize current logs for each source
 	for (const source of logSources) {
 		currentLogs.push(await source.popAsync())
 	}
 
-	while (!isAllDrained) {
-		let earliestLogIndex = -1
-		let earliestLogDate = Infinity
-
-		// find the earliest log entry among current logs
-		for (let i = 0; i < currentLogs.length; i++) {
-			if (currentLogs[i] && currentLogs[i].date < earliestLogDate) {
-				earliestLogIndex = i
-				earliestLogDate = currentLogs[i].date
-			}
-		}
-
-		// if no log entries left in any source, exit loop
-		if (earliestLogIndex === -1) {
-			isAllDrained = true
-			break
-		}
+	while (!allDrained) {
+		const earliestLogIndex = getEarliestLogIndex(currentLogs)
 
 		// print the earliest log entry
 		printer.print(currentLogs[earliestLogIndex])
 
-		// popAsync next log entry from the source and update currentLogs array
+		// reload next logs
 		currentLogs[earliestLogIndex] = await logSources[earliestLogIndex].popAsync()
 
 		// check if all log sources are drained
-		isAllDrained = logSources.every((source) => source.drained)
+		allDrained = isAllDrained(logSources)
 	}
 
 	return console.log("Async sort complete.")
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,45 +1,31 @@
 "use strict";
 
+const { getEarliestLogIndex, isAllDrained } = require('./utils')
+
 // Print all entries, across all of the sources, in chronological order.
-
 module.exports = (logSources, printer) => {
- let currentLogs = []
+	let currentLogs = []
 
-  // tracking if all the log sources is drained
-  let isAllDrained = false
+	// tracking if all the log sources is drained
+	let allDrained = false
 
-  // initialize current logs for each source
-  for (const source of logSources) {
-    currentLogs.push(source.pop())
-  }
+	// initialize current logs for each source
+	for (const source of logSources) {
+		currentLogs.push(source.pop())
+	}
 
-  while (!isAllDrained) {
-    let earliestLogIndex = -1
-    let earliestLogDate = Infinity
+	while (!allDrained) {
+		const earliestLogIndex = getEarliestLogIndex(currentLogs)
 
-    // find the earliest log entry among current logs
-    for (let i = 0; i < currentLogs.length; i++) {
-      if (currentLogs[i] && currentLogs[i].date < earliestLogDate) {
-        earliestLogIndex = i
-        earliestLogDate = currentLogs[i].date
-      }
-    }
+		// print the earliest log entry
+		printer.print(currentLogs[earliestLogIndex])
 
-    // if no log entries left in any source, exit loop
-    if (earliestLogIndex === -1) {
-      isAllDrained = true
-      break
-    }
+		// pop next log entry from the source and update currentLogs array
+		currentLogs[earliestLogIndex] = logSources[earliestLogIndex].pop()
 
-    // print the earliest log entry
-    printer.print(currentLogs[earliestLogIndex])
+		// check if all log sources are drained
+		allDrained = isAllDrained(logSources)
+	}
 
-    // pop next log entry from the source and update currentLogs array
-    currentLogs[earliestLogIndex] = logSources[earliestLogIndex].pop()
-
-    // check if all log sources are drained
-    isAllDrained = currentLogs.every((log) => !log)
-  }
-
-  return console.log("Sync sort complete.")
+	return console.log("Sync sort complete.")
 }
